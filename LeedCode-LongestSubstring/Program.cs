382d485b9f980125869cbbb8e35beeb837a2a0b1
rajesh <rajesh.paddy@gmail.com>
2019-02-03 21:07:50 -0800

    Longest Substring and Add two numbers

---
+++ LeedCode-LongestSubstring/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeedCode_LongestSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int LengthOfLongestSubstring(string s)
+        {
+            string buffer = "";
+            string longeststring = "";
+            foreach (char c in s)
+            {
+
+                if (!buffer.Contains(c))
+                    buffer += c;
+                else
+                {
+                    if (buffer.Length > longeststring.Length)
+                    {
+                        longeststring = buffer;
+                    }
+                    int indexof = buffer.IndexOf(c);
+                    buffer = buffer.Substring(indexof + 1) + c;
+                }
+            }
+            if (buffer.Length > longeststring.Length)
+            {
+                longeststring = buffer;
+            }
+
+            return longeststring.Length;
+        }
+
+        public int LengthOfLongestSubstring_slow(string s)
+        {
+            Dictionary<int, string> maptable = new Dictionary<int, string>();
+            int positioncounter = 1;
+            int eligiblecounterstartingposition = 1;
+            int longeststringpointer = 1;
+            int longeststringpointerlength = 0;
+            int overallloop = 0;
+            foreach (char c in s)
+            {
+                if (!maptable.ContainsKey(positioncounter))
+                    maptable.Add(positioncounter, c.ToString());
+                for (int j = eligiblecounterstartingposition; j < positioncounter; j++)
+                {
+                    if (!maptable[j].Contains(c))
+                    {
+                        maptable[j] += c.ToString();
+                        if (maptable[j].Length > longeststringpointerlength)
+                        {
+                            longeststringpointer = j;
+                            longeststringpointerlength = maptable[j].Length;
+                        }
+                    }
+                    else
+                    {
+                        eligiblecounterstartingposition++;
+                    }
+                    overallloop++;
+                }
+                positioncounter++;
+            }
+            if (longeststringpointerlength == 0 && maptable.Count > 0)
+            {
+                longeststringpointerlength = 1;
+            }
+            return longeststringpointerlength;
+        }
+    }
+}
---
+++ LeetCode-AddTwoNumbers/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace LeetCode_AddTwoNumbers
+{
+    class Program
+    {
+          public class ListNode
+        {
+      public int val;
+      public ListNode next;
+      public ListNode(int x) { val = x; }
+     }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+        static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            int l1Number = 0, l2Number = 0;
+            ListNode returnNode = new ListNode(0);
+            ListNode travelnode = returnNode;
+            int carryover = 0;
+            while (l1 != null || l2 != null)
+            {
+
+                if (l1 != null)
+                {
+                    l1Number = l1.val;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    l1Number = 0;
+                }
+                if (l2 != null)
+                {
+                    l2Number = l2.val;
+                    l2 = l2.next;
+                }
+                else
+                {
+                    l2Number = 0;
+                }
+
+                int finalnumber = l1Number + l2Number + carryover;
+
+
+                carryover = finalnumber / 10;
+                travelnode.val = finalnumber % 10;
+
+                if (l1 != null || l2 != null || carryover > 0)
+                {
+                    travelnode.next = new ListNode(carryover);
+                    travelnode = travelnode.next;
+                }
+
+            }
+
+            return returnNode;
+
+        }
+    }
+}
